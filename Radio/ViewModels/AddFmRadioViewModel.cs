5a5a3885e82dcb99696dbafd6eba7731bf153976
LukasBuchner <lukas.buchner26@gmail.com>
2022-08-04 16:38:29 +0200

    "Save Buttons" only enable once a name has been entered

--- Radio/ViewModels/AddFmRadioViewModel.cs
+++ Radio/ViewModels/AddFmRadioViewModel.cs
@@ -1,7 +1,38 @@
+using System.Reactive;
+using Radio.Models;
+using ReactiveUI;
+
 namespace Radio.ViewModels;
 
 public class AddFmRadioViewModel : ViewModelBase
 {
-    public string Name { get; set; }
-    public string Frequency { get; set; }
+    private string _name;
+    private string _frequency;
+
+    public AddFmRadioViewModel()
+    {
+        var saveEnabled = this.WhenAnyValue(
+            x => x.Name,
+            x => !string.IsNullOrWhiteSpace(x));
+
+        Save = ReactiveCommand.Create(
+            () => new FmRadio { Name = Name, Frequency = Frequency}, 
+            saveEnabled);
+        Cancel = ReactiveCommand.Create(() => { });
+    }
+    
+    public string Name
+    {
+        get => _name; 
+        set => this.RaiseAndSetIfChanged(ref _name, value);
+    }
+
+    public string Frequency
+    {
+        get => _frequency; 
+        set => this.RaiseAndSetIfChanged(ref _frequency, value);
+    }
+
+    public ReactiveCommand<Unit, FmRadio> Save { get; }
+    public ReactiveCommand<Unit, Unit> Cancel { get; }
 }
--- Radio/ViewModels/AddOnlineRadioViewModel.cs
+++ Radio/ViewModels/AddOnlineRadioViewModel.cs
@@ -1,7 +1,38 @@
+using System.Reactive;
+using Radio.Models;
+using ReactiveUI;
+
 namespace Radio.ViewModels;
 
 public class AddOnlineRadioViewModel : ViewModelBase
 {
-    public string Name { get; set; }
-    public string Url { get; set; }
+    private string _name;
+    private string _url;
+
+    public AddOnlineRadioViewModel()
+    {
+        var saveEnabled = this.WhenAnyValue(
+            x => x.Name,
+            x => !string.IsNullOrWhiteSpace(x));
+
+        Save = ReactiveCommand.Create(
+            () => new OnlineRadio() { Name = Name, Url = Url}, 
+            saveEnabled);
+        Cancel = ReactiveCommand.Create(() => { });
+    }
+    
+    public string Name
+    {
+        get => _name; 
+        set => this.RaiseAndSetIfChanged(ref _name, value);
+    }
+
+    public string Url
+    {
+        get => _url; 
+        set => this.RaiseAndSetIfChanged(ref _url, value);
+    }
+    
+    public ReactiveCommand<Unit, OnlineRadio> Save { get; }
+    public ReactiveCommand<Unit, Unit> Cancel { get; }
 }

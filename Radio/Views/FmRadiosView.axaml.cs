d22e27f67cfecb27b1846c4696715a7d64affcc1
LukasBuchner <lukas.buchner26@gmail.com>
2022-08-04 10:17:24 +0200

    has some weird error

---
+++ Radio/Views/FmRadiosView.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Radio.Views;
+
+public partial class FmRadiosView : UserControl
+{
+    public FmRadiosView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ Radio/Views/OnlineRadiosView.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Radio.Views;
+
+public partial class OnlineRadiosView : UserControl
+{
+    public OnlineRadiosView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}

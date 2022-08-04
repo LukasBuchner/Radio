e65dac03217f6be9e995f4eb66ca8c0a80fdb9a5
LukasBuchner <lukas.buchner26@gmail.com>
2022-08-04 16:12:35 +0200

    Add views can be opened

--- Radio/ViewModels/MainWindowViewModel.cs
+++ Radio/ViewModels/MainWindowViewModel.cs
@@ -2,16 +2,32 @@
 using System.ComponentModel;
 using Radio.Models;
 using Radio.Services;
+using ReactiveUI;
 
 namespace Radio.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private ViewModelBase _currentViewModel;
+
     public MainWindowViewModel(Database database)
     {
-        CurrentViewModel = RadiosViewModel = new RadiosViewModel(database);
+        CurrentViewModel = new RadiosViewModel(database);
     }
-    
-    public ViewModelBase CurrentViewModel { get; }
-    public RadiosViewModel RadiosViewModel { get; }
+
+    public ViewModelBase CurrentViewModel
+    {
+        get => _currentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _currentViewModel, value);
+    }
+
+    public void AddFmRadio()
+    {
+        CurrentViewModel = new AddFmRadioViewModel();
+    }
+
+    public void AddOnlineRadio()
+    {
+        CurrentViewModel = new AddOnlineRadioViewModel();
+    }
 }
